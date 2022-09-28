793d7dfd0829917577b77e58f2ee1a9db3ef954f
aomona <104058728+aomona@users.noreply.github.com>
2022-09-28 16:33:18 +0900

    beta 0.1

--- mathgame/game.py
+++ mathgame/game.py
@@ -1,2 +1,7 @@
 import random
-print(random.randint(1,10))
+from re import S
+value = [random.randint(1,10),random.randint(1,10)]
+answer = value[0] + value[1]
+p_answer = input(str(value[0]) + "+" + str(value[1]) + "=")
+if answer == int(p_answer):
+    print ("正解！")
