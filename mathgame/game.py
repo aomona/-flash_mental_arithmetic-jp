f510eef1e3188c2dfa3c7c006075168da9f80c97
aomona <104058728+aomona@users.noreply.github.com>
2022-09-29 10:42:50 +0900

    ver 1.0 rerease

--- mathgame/game.py
+++ mathgame/game.py
@@ -1,17 +1,30 @@
 import random
 from re import S
+from time import sleep
 print ("難易度を選択してください")
-print ("1を選ぶと1ケタ,2を選ぶと1~2ケタ,3を選ぶと1~3ケタ")
+print ("1を選ぶと簡単,2を選ぶと普通,3を選ぶと難しいの難易度になります")
 difficulty = input("1~3の数字を入力してください:")
 if int(difficulty) == 1:
     digits = 9
+    print ("難易度が「簡単」に設定されました")
 elif int(difficulty) == 2:
     digits = 99
+    print ("難易度が「普通」に設定されました")
 elif int(difficulty) == 3:
     digits = 999
+    print ("難易度が「難しい」に設定されました")
 else:
     print ("難易度の取得に失敗しました、正しい数値を入力してもう一度お試しください")
+    print ("このゲームはまもなく終了します")
+    sleep(3)
 if int(difficulty) == 1 or int(difficulty) == 2 or int(difficulty) == 3:
+    print ("3")
+    sleep(1)
+    print ("2")
+    sleep(1)
+    print (1)
+    sleep(1)
+    print ("スタート！！")
     for i in range(5):
         value = [random.randint(1,digits),random.randint(1,digits)]
         answer = value[0] + value[1]
@@ -22,4 +35,5 @@ if int(difficulty) == 1 or int(difficulty) == 2 or int(difficulty) == 3:
             print ("不正解!残念!")
     else:
         print ("お疲れ様でした！")
-
+        print ("このゲームはまもなく終了します")
+        sleep(3)
